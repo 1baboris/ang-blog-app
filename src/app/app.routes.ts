8b5fca423c8e3a14e2e64b287aa9c41061d5bf25
baboris <boris.benachour@gmail.com>
2024-05-14 20:22:18 +0200

    Configuration des Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,35 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { SigleCategoryComponent } from './pages/sigle-category/sigle-category.component';
+import { SiglePostComponent } from './pages/sigle-post/sigle-post.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'category',
+    component: SigleCategoryComponent,
+  },
+  {
+    path: 'post',
+    component: SiglePostComponent,
+  },
+
+  {
+    path: 'about',
+    component: AboutUsComponent,
+  },
+  {
+    path: 'term-condition',
+    component: TermsAndConditionComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactUsComponent,
   },
 ];
